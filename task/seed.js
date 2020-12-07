e1a8f0b57e70ef1710622666890ea2f844d41ba7
nrajan-sit <72529770+nrajan-sit@users.noreply.github.com>
2020-12-06 19:27:53 -0600

    setting up things

---
+++ task/seed.js
@@ -0,0 +1,13 @@
+const dbConnection = require('../config/mongoConnection');
+const data = require('../data/');
+
+
+const main = async () => {
+  const db = await dbConnection();
+  await db.dropDatabase();
+
+  console.log('Done seeding database');
+  await db.serverConfig.close();
+};
+
+main().catch(console.log);
